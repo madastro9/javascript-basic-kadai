f78f7d2048288c169c50e0f795437377283caaee
madastro9 <shinaifantian680@gmail.comgit clone https://github.com/madastro9/git-basic-kadai.git[200~git clone https://github.com/madastro9/git-basic-kadai.gitgit config --global user.email shinaifantian680@gmail.com>
2024-06-18 20:07:24 +0900

    課題再提出のコミット

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,22 +1,19 @@
+// 変数numに0～15までのランダムな整数を代入する
+let num = Math.floor(Math.random() * 16);
 
-let num = Math.floor(Math.random() * 20);
-
-//numを3で割ったらあまりが0になる＝3の倍数
-if (num % 3 == 0) {
+// 先に3と5の倍数の場合をチェックする
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log('3と５の倍数です');
+}
+// 次に3の倍数だけをチェックする
+else if (num % 3 === 0) {
   console.log('3の倍数です');
 }
-
-//numを5で割ったらあまりが0になる＝5の倍数
-else if (num % 5 == 0) {
+// その後で5の倍数だけをチェックする
+else if (num % 5 === 0) {
   console.log('5の倍数です');
 }
-
-//numを3または5で割ったらあまりが0になる＝3と5の倍数
- if (num % 3 == 0 && num % 5 == 0) {
-  console.log('3と5の倍数です');
-}
-
-//それ以外の場合の変数の値を出力する
+// それ以外の場合、numの値をそのまま出力する
 else {
   console.log(num);
 }
